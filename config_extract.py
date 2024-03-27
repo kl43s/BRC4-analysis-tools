d51e8f59f379ef23cb1efca0a7376ffb85b0741b
kl43s <58507201+kl43s@users.noreply.github.com>
2024-03-27 12:31:28 +0100

    Update config_extract.py

--- config_extract.py
+++ config_extract.py
@@ -1,18 +1,19 @@
 import idautils
 import idc
 import idaapi
-import time 
+import time
 
-
-print('''###############
+print(
+    """###############
 #    START    #
 ###############
-''')
+"""
+)
 
 for func in idautils.Functions():
     for instruction in idautils.Heads(func, idc.get_func_attr(func, idc.FUNCATTR_END)):
         True
-        
+
 instruction += 1
 start_address = instruction
 
@@ -27,16 +28,15 @@ for i, addr in enumerate(idautils.Heads(start_address, ida_ida.cvar.inf.max_ea))
         break
     else:
         instructions.append(hex(addr) + ": " + mnem + " " + operand)
-        
 
 call_func = []
 for i in instructions:
     if "call" in i:
-        if "syscall" in i.split(': ')[1]:
-            call_func.append(i.split(':')[0])
-        elif len(i.split(': ')[1]) == 8:
-            call_func.append(i.split(':')[0])
-            
+        if "syscall" in i.split(": ")[1]:
+            call_func.append(i.split(":")[0])
+        elif len(i.split(": ")[1]) == 8:
+            call_func.append(i.split(":")[0])
+
 
 addr_to_break = call_func[-1]
 time.sleep(2)
@@ -45,8 +45,6 @@ idaapi.add_bpt(int(addr_to_break, base=16), 0, idaapi.BPT_DEFAULT)
 idaapi.load_debugger("windbg", 1)
 idaapi.start_process(ida_nalt.get_input_file_path(), "", "")
 idaapi.continue_process()
-
-
 ida_dbg.wait_for_next_event(ida_dbg.WFNE_SUSP, -1)
 rsp_value = idaapi.get_reg_val("RSP")
 
@@ -57,12 +55,14 @@ while hex(addr) != "0x20000":
     addr = struct.unpack("<Q", addr)[0]
     print(hex(addr))
 
-print( idaapi.get_bytes(addr, 256) )
+print(idaapi.get_bytes(addr, 256))
 
 idaapi.exit_process()
 
 
-print('''#############
+print(
+    """#############
 #    END    #
 #############
-''')
+"""
+)
